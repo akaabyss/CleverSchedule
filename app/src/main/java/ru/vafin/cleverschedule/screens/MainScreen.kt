f9e539da0f21e8d2de3522f414c36398ba8bcce7
Softpaw <vaf_art@mail.ru>
2024-01-04 11:14:24 +0300

    Fix the bug with date in the MainScreen, replace info about developer

--- app/src/main/java/ru/vafin/cleverschedule/screens/MainScreen.kt
+++ app/src/main/java/ru/vafin/cleverschedule/screens/MainScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
@@ -21,6 +20,7 @@ import androidx.compose.material.icons.filled.Autorenew
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -33,7 +33,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -104,19 +103,35 @@ fun MainScreen(
                     horizontalArrangement = Arrangement.SpaceEvenly,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Image(
-                            painter = painterResource(id = R.drawable.softpaw),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .clip(CircleShape)
-                                .size(30.dp) //форма
-                        )
-                        Text(text = "@akasoftpaw", fontSize = 12.sp)
-                    }
 
+                    var isExpandedInfoAboutMe by remember {
+                        mutableStateOf(false)
+                    }
+                    Box {
+                        IconButton(onClick = { isExpandedInfoAboutMe = true }) {
+                            Icon(
+                                imageVector = Icons.Filled.Info,
+                                contentDescription = "Info about me",
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
+                        DropdownMenu(
+                            expanded = isExpandedInfoAboutMe,
+                            onDismissRequest = { isExpandedInfoAboutMe = false }) {
+                            Column(modifier = Modifier.padding(5.dp)) {
+                                Text(
+                                    text = "@akaabyss",
+                                    fontSize = 20.sp
+                                )
+                            }
+                        }
+                    }
                     Text(
-                        "${localDateTime.dayOfMonth}.${localDateTime.month.value}",
+                        text = "${localDateTime.dayOfMonth}." + if (localDateTime.month.value < 10) {
+                            "0${localDateTime.month.value}"
+                        } else {
+                            localDateTime.month.value
+                        },
                         fontSize = 22.sp
                     )
                 }
